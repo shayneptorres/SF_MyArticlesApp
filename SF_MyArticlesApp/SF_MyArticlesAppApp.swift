f59b273a4385c21943000c0a1b1d27455357e1c9
Shayne Torres <shayneptorres@gmail.com>
2024-04-11 15:08:47 -0700

    Initial Commit

---
+++ SF_MyArticlesApp/SF_MyArticlesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SF_MyArticlesAppApp.swift
+//  SF_MyArticlesApp
+//
+//  Created by Shayne Torres on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SF_MyArticlesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
